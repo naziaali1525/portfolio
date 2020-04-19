3a7a22f45454146611b6f9295667b6c1e605897f
naziaali1525 <naziaali688@gmail.com>
2020-04-19 14:33:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showIcon(){
+    var x = document.getElementById('show-list');
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+    
+    console.log("hello");
+}
+var y = " yess";
+
+if (y == "no"){
+    console.log("hi");
+}else{
+    console.log("hello");
+}
